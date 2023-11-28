7ca20d2d715101501c7aacc0d15a3a82d5a1310e
Jonas Soderholm <jonas.soderholm@finova.tech>
2023-11-28 14:29:29 +0000

    Add project files.

---
+++ SerilogOwinBug/SerilogMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin;
+using Serilog.Context;
+using Serilog;
+using System.Threading.Tasks;
+
+namespace SerilogOwinBug
+{
+    public class SerilogMiddleware : OwinMiddleware
+    {
+        public SerilogMiddleware(OwinMiddleware next) : base(next)
+        {
+
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            using (LogContext.PushProperty("RequestId", "123"))
+            {
+                Log.Information("Starting middleware. URL: {URL}", context.Request.Path);
+                await Next.Invoke(context);
+                Log.Information("Ending middleware. URL: {URL}", context.Request.Path);
+            }
+        }
+    }
+}
---
+++ SerilogOwinBug/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Serilog;
+
+[assembly: OwinStartup(typeof(SerilogOwinBug.Startup))]
+
+namespace SerilogOwinBug
+{
+    public class Startup
+    {
+        // Running the app produces the following log:
+        // 2023-11-28 14:25:35Z [RequestId: 123] Starting middleware. URL: "/"
+        // 2023-11-28 14:25:35Z [RequestId: ] Hello from Home Controller
+        // 2023-11-28 14:25:37Z [RequestId: 123] Ending middleware. URL: "/"
+        //
+        // Expecting
+        // 2023-11-28 14:25:35Z [RequestId: 123] Starting middleware. URL: "/"
+        // 2023-11-28 14:25:35Z [RequestId: 123] Hello from Home Controller
+        // 2023-11-28 14:25:37Z [RequestId: 123] Ending middleware. URL: "/"
+        public Startup()
+        {
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Trace(
+                    outputTemplate: "{Timestamp:u} [RequestId: {RequestId}] {Message}"
+                 )
+                .CreateLogger();
+        }
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use<SerilogMiddleware>();
+        }
+    }
+}
